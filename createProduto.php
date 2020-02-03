fc6553e8b3b1808569c2905c7fc2be2cb59cae23
verissteph <stephanieverissimopp@gmail.com>
2020-02-03 13:44:39 -0300

    Inicio do formulario

---
+++ createProduto.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<?php
+
+
+?>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">
+                < Desafio PHP />
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Adicionar produto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Usuários</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="navbar-brand" href="#">Logout</a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+    </header>
+
+    <!-- <form>
+        <div class="form-group">
+            <label for="exampleInputtext1">Nome do produto</label>
+            <input type="text" class="form-control" id="exampleInputtext1" aria-describedby="produtoHelp" placeholder="Qual o seu produto?">
+        </div>
+
+        <div class="form-group">
+            <label for="exampleInputPassword1">Senha</label>
+            <input type="textarea" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+        </div>
+
+        <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Clique em mim</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form> -->
+</body>
+
+</html>
