2d8e798c90d7acfc8df30adcc05a27461a23db44
verissteph <stephanieverissimopp@gmail.com>
2020-02-03 18:57:49 -0300

    Formulario atualizado novamente, usando listas

--- createProduto.php
+++ createProduto.php
@@ -10,40 +10,69 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Cadastro Produtos</title>
 </head>
 
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">
-                < Desafio PHP />
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Adicionar produto</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Usuários</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="navbar-brand" href="#">Logout</a>
-                    </li>
-                </ul>
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    < Desafio PHP /> </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link " href="#">Adicionar produto</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link " href="#">Usuários</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link  " href="#">Logout</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     </header>
+    <div class="container ">
+        <span>
+            <h1> Adicionar Produto </h1>
+        </span>
+        <form>
+            <div class="row">
+                <div class="col">
+                    <label for="form-control">Nome</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col">
+                    <label for="form-control">Preço</label>
+                    <input type="text" class="form-control">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Descrição</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+            </div>
+            <div class="custom-file">
+                <input type="file" class="custom-file-input" id="customFile">
+                <label class="custom-file-label" for="customFile">Selecione a foto</label>
+            </div>
+            <div class="button-add py-3">
+                <button type="button" class="btn btn-info btn-block">Adicionar</button>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
