70c0faf580fe776063dcfec6c48748952e7a8c76
verissteph <stephanieverissimopp@gmail.com>
2020-02-20 17:39:51 -0300

    conclusão do codigo de validação

--- createProduto.php
+++ createProduto.php
@@ -1,7 +1,5 @@
-<!DOCTYPE html>
-
 <?php
-
+session_start();
 // var_dump($_FILES);
 //Validando campos
 
@@ -28,62 +26,46 @@ if (($_FILES) && ($_POST)) {
         }
     }
 }
-?>
 
-<?php
-//ARQUIVO JSON
+//--------------------------------------------------JSON------------------------------------------
 if (!empty($_POST) && empty($array_erro)) {
     // lógica de que não pode enviar info se todos os campos obrigatorios estiverem vazios
     // recebendo os POSTS
     $cadastro = $_POST;
-      // le arquivo
+    // le arquivo
     $le_arq = file_get_contents('dadosProduto.json');
     $armazena_decode = json_decode($le_arq, true); //ele vai transformar em array assoc
     //Condição caso o arqv json esteja vazio, ele vai atribuir ao indice ID 
-    if($armazena_decode==null){
-            $cadastro['id']=1;  
-    }else{
-            $cadastro['id'] = count($armazena_decode)+1;
-        }
+    if ($armazena_decode == null) {
+        $cadastro['id'] = 1;
+    } else {
+        $cadastro['id'] = count($armazena_decode) + 1;
+    }
     //$cadastro['imagem'] = $_FILES['upload']['name'];
     //mostra infos do arquivo
     $info = pathinfo($_FILES['upload']['name']);
     //mostra qual extensao do arquivo 
     $extension = $info['extension'];
     //renomeando o arquivo 
-    $nome_imagem = $img_name= $info['filename']."-".$cadastro['id'].'.'.$extension;
-    
-    // var_dump($nome_imagem); exit;
-    
+    $nome_imagem = $img_name = $info['filename'] . "-" . $cadastro['id'] . '.' . $extension;
+
     //inserir conteudo do cadastro do produto do formulario no array 
     $armazena_decode[] = $cadastro;
     //transformar novamente em JSON 
-    $conteudo_cadastro = json_encode($armazena_decode);
+    $conteudo_cadastro = json_encode($armazena_decode, JSON_PRETTY_PRINT);
     //guarda o conteudo ''string'' no arquivo JSON
     $armazena_arq = file_put_contents('dadosProduto.json', $conteudo_cadastro);
     //Para armazenar a imagem na pasta de imagem:
     if (is_dir('img/')) {
-         move_uploaded_file($_FILES['upload']['tmp_name'], 'img/' . $nome_imagem);
-     } else {
-         mkdir('img/');
-         move_uploaded_file($_FILES['upload']['tmp_name'], 'img/' . $nome_imagem);
-     }
+        move_uploaded_file($_FILES['upload']['tmp_name'], 'img/' . $nome_imagem);
+    } else {
+        mkdir('img/');
+        move_uploaded_file($_FILES['upload']['tmp_name'], 'img/' . $nome_imagem);
+    }
 }
-
-
-
-//TESTE PARA VER SE A IMAGEM APARECE
-// $arquivo = $_FILES['uploa']['tmp_nome'];
-// pega arq
-// $pega_foto= file_get_contents('dadosProduto.json');
-// $armazena_decode = json_decode($pega_foto,true);
-// $armazena_decode[] = $arquivo;
-// $foto_cadastro = json_encode($armazena_decode);
-// $foto_armazenada = file_put_contents('dadosProduto.json',$foto_cadastro);
-
 ?>
 
-
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -95,37 +77,7 @@ if (!empty($_POST) && empty($array_erro)) {
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    < Desafio PHP /> </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link " href="#">Adicionar produto</a>
-                        </li>
-                        <li class="nav-item active">
-                            <a class="nav-link " href="#">Usuários</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link  " href="#">Logout</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+
     <div class="container ">
         <span>
             <h1> Adicionar Produto </h1>
@@ -157,7 +109,7 @@ if (!empty($_POST) && empty($array_erro)) {
                 <label class="custom-file-label" for="customFile">Selecione a foto</label>
             </div>
             <div class="button-add py-3">
-                <button type="submit" class="btn btn-info btn-block">Adicionar</button>
+                <button type="submit" class="btn btn-info btn-block" name="adicionar">Adicionar</button>
             </div>
         </form>
     </div>
