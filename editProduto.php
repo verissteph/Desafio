a5ad15df64aee8a12ed9c9d2cd91cd449e52e3f1
verissteph <stephanieverissimopp@gmail.com>
2020-02-26 16:00:54 -0300

    Tentando rodar a lógica

--- editProduto.php
+++ editProduto.php
@@ -1,6 +1,59 @@
 <?php
 session_start();
 //primeiro: qnd apertar o botao de editar produto na lista deve encaminhar para esta página e trazer informação que tem guardada no Json
+// echo("<pre>");
+// var_dump($_SERVER);
+// echo("</pre>");
+$armazena_decode=[];
+
+//$le_arq = file_get_contents('dadosProduto.json'); //pega arq json
+//$armazena_decode = json_decode($le_arq, true); //transf em array
+//if (isset($_GET['id'])) {
+    
+    //$posicao_produto = array_search($_POST['id'], array_column($armazena_decode, 'id')); //pesquisando a posição do produto pelo ID 
+//}
+// if($_SERVER['REQUEST_METHOD'] === $_POST){
+//     //modifica os dados.
+//     $le_arq = file_get_contents('dadosProduto.json'); //pega arq json
+//     $armazena_decode = json_decode($le_arq, true); //transf em array  
+//     $produto_att=[];
+//     $produtos = $armazena_decode;
+//         foreach($produtos as $indice => $produto){
+//             if($produto['id']==$_GET['id']){
+//                 $produtos[$indice] = $produto_att = array_merge($produto,$_POST);
+//             }
+//         }
+//     $salva_produto=file_put_contents('dadosProduto.json',$produtos,);
+// echo  $salva_produto;
+// }
+
+//---------------------------------VALIDAÇÃO CAMPOS---------------------------------
+if (isset($_POST['edit'])) {
+    if (($_FILES) && ($_POST)) {
+        $array_erro = [];
+
+        if (empty($_POST['preco']) && empty($_POST['nome']) && empty($_FILES['upload']['tmp_nome'])) {
+            $array_erro[] = 'ERRO - Preencha os campos.';
+        } else {
+            $preco = $_POST['preco'];
+            if (empty($preco)) {
+                $array_erro[] = 'ERRO - Inclua o preço.';
+            } else if (!is_numeric($preco)) {
+                $array_erro[] = "ERRO - O preço deve conter apenas números.";
+            }
+            $nome = $_POST['nome'];
+            if (empty($nome)) {
+                $array_erro[] = "ERRO - Inclua o nome do produto.";
+            }
+
+            $foto = $_FILES['upload']["tmp_name"];
+            if (!$foto) {
+                $array_erro[] = "ERRO - Inclua uma foto.";
+            }
+        }
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,24 +75,25 @@ session_start();
         <form method="POST">
             <div class="row">
                 <div class="col">
+                    <input type="hidden" class="form-control" name="id" value="<?php echo  $armazena_decode['id']; ?>">
                     <label for="form-control">Nome</label>
-                    <input type="text" class="form-control" name="nome" required>
+                    <input type="text" class="form-control" name="nome" value="<?php echo  $armazena_decode['nome']; ?>" >
                 </div>
                 <div class="col">
                     <label for="form-control">Preço</label>
-                    <input type="text" class="form-control" name="preco">
+                    <input type="text" class="form-control" name="preco" value="<?php echo  $armazena_decode['preco']; ?>">
                 </div>
             </div>
             <div class="form-group">
                 <label for="exampleFormControlTextarea1">Descrição</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="10" name="descricao"></textarea>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="10" name="descricao" value="<?php echo  $armazena_decode['descricao']; ?>"></textarea>
             </div>
             <div class="imagem my-3">
-                <!-- Ajustar a imagem -->
+                <!-- Ajustar a imagem e mostrar aqui.-->
                 <img src="" class="img-fluid rounded max-width: 100%">
             </div>
             <div class="custom-file">
-                <input type="file" class="custom-file-input" id="customFile" name="upload" required>
+                <input type="file" class="custom-file-input" id="customFile" name="upload" >
                 <label class="custom-file-label" for="customFile">Selecione a foto</label>
             </div>
             <div class="button-add py-3">
