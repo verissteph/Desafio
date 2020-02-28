35ee75f15aad7e63f850efadb7c5c35490adef3c
verissteph <stephanieverissimopp@gmail.com>
2020-02-28 20:26:12 -0300

    Deletando pelo ID. FINALIZADO

--- deletarUsuario.php
+++ deletarUsuario.php
@@ -2,14 +2,15 @@
 
 $armazena_json = file_get_contents('dadosUsuario.json'); // peguei os dados do Json
 $meujson_deco = json_decode($armazena_json, TRUE); // retornando um array e não um objeto
-$posicao_userId = array_search($_GET['id'], array_column($meujson_deco, 'id'));
+//$posicao_userId = array_search($_GET['id'], array_column($meujson_deco, 'id'));
+
 
 foreach ($meujson_deco as $indice => $usuarios) {
     if ($usuarios['id'] == $_GET['id']) {
         array_splice($meujson_deco, $indice, 1);
     }
 }
-    $array_json = json_encode($meujson_deco, JSON_PRETTY_PRINT);
-    $novo_json_del_usuario = file_put_contents('dadosProduto.json', $array_json);
+    
+    $novo_json_del_usuario = file_put_contents('dadosUsuario.json', json_encode($meujson_deco, JSON_PRETTY_PRINT));
     header("Location: createUsuario.php");
 ?>
