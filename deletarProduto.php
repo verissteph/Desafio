90564166e0b4c3e2c631db1084d69b7dd877b234
verissteph <stephanieverissimopp@gmail.com>
2020-02-28 20:34:58 -0300

    Deleta o produto, mas a imagem relacionada ao mesmo continua armazenada na pasta. Solução não encontrada.

--- deletarProduto.php
+++ deletarProduto.php
@@ -4,10 +4,21 @@ $dados_produtos = file_get_contents('dadosProduto.json');
 $array_produtos = json_decode($dados_produtos, true);
 
 foreach ($array_produtos as $indice => $produtos) {
+    // if (($produtos['foto'])) {
+    //     $caminho = "/img".date("ymdHis") . '-' . $_FILES['uploadedit']['name'];
+    //     unlink($$caminho);
+ //Não está funcionando para deletar a foto da pasta img. A logica parece certa.
     if ($produtos['id'] == $_GET['id']) {
         array_splice($array_produtos, $indice, 1);
     }
 }
+
+
+    // echo ("<pre>");
+    // print_r($array_produtos);
+    // echo ("</pre>");
+    // exit;
+
     $array_json = json_encode($array_produtos, JSON_PRETTY_PRINT);
     $novo_json_del = file_put_contents('dadosProduto.json', $array_json);
     header("Location: indexProduto.php");
